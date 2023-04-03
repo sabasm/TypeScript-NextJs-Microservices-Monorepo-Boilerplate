1372d28ff883e60b228ef0421bc2b17e1b8f3bf0
Sabas Mendivil <user.name>
2023-04-03 02:58:54 +0000

    removing log

--- web-client/src/pages/_app.tsx
+++ web-client/src/pages/_app.tsx
@@ -4,7 +4,6 @@ import { ThemeProvider } from 'styled-components';
 import { darkTheme } from '@/layouts/themes';
 
 export default function App({ Component, pageProps }: AppProps) {
-  console.log(' on App- dark theme = ', darkTheme)
   return (
     <>
       <ThemeProvider theme={darkTheme}>
