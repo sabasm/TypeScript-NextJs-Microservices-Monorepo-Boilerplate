51d113bd40c5f95fe064dc130582271dbfcde8bc
Sabas Mendivil <user.name>
2023-04-07 04:02:41 +0000

    adding constant lib for pages and routes for dynamic handling

---
+++ web-client/src/lib/constants/PAGES.ts
@@ -0,0 +1,20 @@
+enum PUBLIC_PAGES {
+  Home = "/",
+  About = "/about",
+  Contact = "/contact",
+  NotFound = "/404",
+  ServerError = "/500",
+  PrivacyPolicy = "/privacy-policy",
+  TermsOfService = "/terms-of-service",
+  CookiePolicy = "/cookie-policy",
+}
+
+enum PRIVATE_PAGES {
+  Test = "/proof_of_concept",
+  Dashboard = "/dashboard",
+  Profile = "/profile",
+  Settings = "/settings",
+  Admin = "/admin",
+}
+
+export { PUBLIC_PAGES, PRIVATE_PAGES };
--- web-client/src/pages/index.tsx
+++ web-client/src/pages/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Link from 'next/link';
 import { Hero } from '@/layout/components';
+import { PRIVATE_PAGES as Page } from '@/lib/constants';
 
 const Home = () => {
   return (
     <Hero>
       <h1>Homepage</h1>
       <h2>this is an h2</h2>
-      <Link href="/test">Go to Test page</Link>
+      <Link href={Page.Test}>Go to Test page</Link>
     </Hero>
   );
 };
