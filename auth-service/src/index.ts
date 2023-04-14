81f574a7be7ab63f68c369169a54f00ab619359f
Sabas Mendivil <user.name>
2023-04-14 21:13:00 +0000

    bases for express app

--- auth-service/src/index.ts
+++ auth-service/src/index.ts
@@ -1,13 +1,44 @@
-import express from "express";
-import { sayHello } from "./controller";
+"use strict";
+import config from "./config";
+import express, { Express, Request, Response, NextFunction } from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan, { FormatFn } from "morgan";
+import bodyParser from "body-parser";
+import compression from "compression";
+import logger from "./utils/logger";
 
-const app = express();
-const port = 4440;
+const customFormat: FormatFn<Request, Response> = (req, res) => {
+  const message = `${req.method} ${req.url} ${
+    res.statusCode
+  } ${new Date().toLocaleString("en-US", { timeZone: "America/Los_Angeles" })}`;
+  logger.info(message);
+  return message;
+};
 
-app.use(express.json());
+const app: Express = express();
 
-app.get("/users", sayHello);
+// Middleware
+app.use(cors());
+app.use(helmet());
+app.use(compression());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan(customFormat));
 
-app.listen(port, () => {
-  console.log(`Server running on port http://localhost:${port}`);
+// Routes
+app.get("/", (req: Request, res: Response) => {
+  res.send("Welcome to the Auth Service!");
+});
+
+// Error handling
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  logger.error("Something went wrong!");
+  logger.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+// Start server
+app.listen(config.port, () => {
+  logger.info(`Server is listening to your requests...`);
 });
