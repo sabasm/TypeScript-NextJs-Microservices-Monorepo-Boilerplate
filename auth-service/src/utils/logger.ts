68675a473fb0b6494ef042370f7cf701c6775bf1
Sabas Mendivil <user.name>
2023-04-14 22:02:41 +0000

    adding jest testing to auth-service

--- auth-service/src/utils/logger.ts
+++ auth-service/src/utils/logger.ts
@@ -1,16 +1,17 @@
+import config from "../config";
 import winston from "winston";
 
 const logger = winston.createLogger({
   level: "info",
   format: winston.format.json(),
-  defaultMeta: { service: "auth-service" },
+  // defaultMeta: { service: "auth-service" },
   transports: [
     new winston.transports.File({ filename: "error.log", level: "error" }),
     new winston.transports.File({ filename: "combined.log" }),
   ],
 });
 
-if (process.env.NODE_ENV !== "production") {
+if (config.nodeEnv !== "production") {
   logger.add(
     new winston.transports.Console({
       format: winston.format.simple(),
